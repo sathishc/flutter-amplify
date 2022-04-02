6581224c69b65b0e29a91a661b215ec608aa1518
Sathish <sat.hariharan@gmail.com>
2022-04-02 11:30:40 +0530

    remove amplify references

--- lib/todo/todo_service.dart
+++ lib/todo/todo_service.dart
@@ -1,21 +1,17 @@
 import 'dart:typed_data';
 import 'dart:convert';
-import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:amplify_api/amplify_api.dart';
 
 import 'todo.dart';
 
-Future<bool> insertTodo (Todo todo) async {
+Future<bool> insertTodo(Todo todo) async {
   return true;
 }
 
-Future<bool> deleteTodo (Todo todo) async {
-
+Future<bool> deleteTodo(Todo todo) async {
   return true;
 }
 
 Future<List<Todo>> fetchAllTodos(userId) async {
-
   List<Todo> todoList = [];
   return todoList;
 }
